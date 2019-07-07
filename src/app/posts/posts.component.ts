7ba039db3202dd7e8e5ed437133818e978b6b036
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-07-07 13:47:33 +0530

    using async pipe

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostsService } from './service/posts.service';
+import { Subscription, Observable } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+import { IPosts } from './iposts';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
+
+  isValid = true;
+  sub: Subscription;
+
+  posts$: Observable<IPosts[]>;
 
   constructor(private postsService: PostsService) { }
 
   ngOnInit() {
-    this.postsService.getPosts().subscribe(
-      (data) => console.log(data)
-    );
+    // this.postsService.getPosts()
+    //   .pipe(
+    //     takeWhile(() => this.isValid === true)
+    //   ).subscribe(
+    //     (data) => console.log(data)
+    //   );
+    this.posts$ = this.postsService.getPosts();
+  }
+
+  ngOnDestroy(): void {
+    // if (this.sub) {
+    console.log('Unsubscription called');
+    //   this.sub.unsubscribe();
+    // }
+    this.isValid = false;
   }
 
 }
