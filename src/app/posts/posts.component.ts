40bcf3b5b9354949f565cfcc81953d502ef1cdda
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-08-03 14:12:15 +0530

    added tap and http header

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostsService } from './service/posts.service';
 import { Subscription, Observable } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
+import { takeWhile, shareReplay } from 'rxjs/operators';
 import { IPosts } from './iposts';
 
 @Component({
@@ -9,31 +9,31 @@ import { IPosts } from './iposts';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit, OnDestroy {
+export class PostsComponent {
 
   isValid = true;
   sub: Subscription;
 
-  posts$: Observable<IPosts[]>;
+  posts$ = this.postsService.getPosts();
 
   constructor(private postsService: PostsService) { }
 
-  ngOnInit() {
-    // this.postsService.getPosts()
-    //   .pipe(
-    //     takeWhile(() => this.isValid === true)
-    //   ).subscribe(
-    //     (data) => console.log(data)
-    //   );
-    this.posts$ = this.postsService.getPosts();
-  }
+  // ngOnInit() {
+  //   // this.postsService.getPosts()
+  //   //   .pipe(
+  //   //     takeWhile(() => this.isValid === true)
+  //   //   ).subscribe(
+  //   //     (data) => console.log(data)
+  //   //   );
+  //   this.posts$ = this.postsService.getPosts();
+  // }
 
-  ngOnDestroy(): void {
-    // if (this.sub) {
-    console.log('Unsubscription called');
-    //   this.sub.unsubscribe();
-    // }
-    this.isValid = false;
-  }
+  // ngOnDestroy(): void {
+  //   // if (this.sub) {
+  //   console.log('Unsubscription called');
+  //   //   this.sub.unsubscribe();
+  //   // }
+  //   this.isValid = false;
+  // }
 
 }
--- src/app/posts/service/posts.service.ts
+++ src/app/posts/service/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { IPosts } from '../iposts';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -11,8 +12,13 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
+    const header = new HttpHeaders()
+      .set('teskHeader', 'dfhkdsjhfkjh3253463');
     return this.http.get<IPosts[]>
-      ('https://jsonplaceholder.typicode.com/posts');
+      ('https://jsonplaceholder.typicode.com/posts',
+        { headers: header }).pipe(
+          tap(data => console.log(data))
+        );
   }
 
   addPost(post: IPosts) {
